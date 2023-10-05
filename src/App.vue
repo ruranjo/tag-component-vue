3db942ebda57f62012d59406823135f2b3048d82
ruranjo <rubens.rangel.job@gmail.com>
2023-10-05 12:22:01 -0400

    final

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,25 @@
 </script>
 
 <template>
-  <div>
+  <div class="App">
+    <div class="logo">TAG-COMPONENT</div>
     <InputTag/>
   </div>
 </template>
 
 <style scoped>
+
+  .logo{
+    font-size: 64px ;
+    font-weight: 800;
+    margin: 3rem;
+    color: #34495E;
+  }
+
+  @media (max-width:  460px){
+    .logo{
+      font-size: 32px ;
+    }
+  }
+
 </style>
--- src/components/InputTag.vue
+++ src/components/InputTag.vue
@@ -36,23 +36,110 @@
 </script>
 
 <template>
-    <div class="input-tag">
+    <div class="inputTag">
         <div class="tags">
             <div class="tag" v-for="(tag,index) in tags" :key="index">
-                {{ tag }} <button @click="deletedTag(tag)">X</button>
+                {{ tag }} <button class="btn-x" @click="deletedTag(tag)">X</button>
             </div>
         </div>
         <form @submit.prevent="handleSubmit" action="">
             <input type="text" v-model="currentValue" />
         </form>
-        <div>
-            {{ currentValue }}
-        </div>
-        <button @click="handleClick">
+        
+        <button class="btn" @click="handleClick">
             Agregar
         </button>
     </div>
 </template>
 
 <style scoped>
+.inputTag{
+    display: inline-flex;
+    min-height: 43px;
+}
+
+.tags{
+    display:flex;
+    
+    gap: 3px;
+    padding: 5px;
+}
+.tags .tag{
+    border:  solid 2px #ccc;
+    display: flex;
+    padding: 5px;
+    align-items: center;
+    border-radius: 3px;
+    gap: 5px;
+}
+
+.inputTag input {
+  height: 95%;
+  outline: none;
+  padding: 0 5px;
+}
+
+.btn-x {
+  background-color: transparent;
+  border: none;
+  border-radius: 3px;
+}
+
+.btn{
+    margin-left:0.5rem;
+    padding: 1rem;
+    cursor: pointer;
+    background: #545454;
+    border: none;
+    color: aliceblue;
+    font-weight: 800;
+    border-radius: 3px;
+}
+
+.btn-x:hover {
+  background-color: #ccc;
+}
+
+@media  (max-width: 460px) {
+    .inputTag{
+        margin: 0;
+        padding: 0;
+        justify-content: center;
+        display: flex;
+        flex-direction: column;
+        min-height: 43px;
+    }
+    .inputTag input {
+    height: 50px;
+    width: 100%;
+    outline: none;
+    padding: 0 5px;
+    }
+    .tags .tag{
+        justify-content: space-between;
+
+    }
+
+    .tags{
+        flex-direction: column;    
+    }
+}
+
+
+
+
+
+
+
+/*
+
+.inputTag button {
+  background-color: transparent;
+  border: none;
+  border-radius: 3px;
+}
+.inputTag button:hover {
+  background-color: #ccc;
+}
+*/
 </style>
